368029a633593525d160dd8c3664c90b242df75f
David Sok <sokd88@gmail.com>
2015-04-08 15:57:49 -0400

    increment decrement points, display clicked name

--- leaderboard.js
+++ leaderboard.js
@@ -4,20 +4,33 @@ if(Meteor.isClient){
   //this code only runs on the client
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayerList.find()
+      return PlayerList.find({}, {sort: {score: -1, name: -1}})
     },
     'selectedClass' : function(){
       var playerId = this._id;
-      var selectedPlayer = Session.get('selectedPlayer')
+      var selectedPlayer = Session.get('selectedPlayer');
       if(playerId==selectedPlayer){
         return "selected"
       }
+    },
+    'showSelectedPlayer' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayerList.findOne(selectedPlayer);
     }
   });
+
   Template.leaderboard.events({
     'click .player' : function(){
         var playerId = this._id;
         Session.set('selectedPlayer', playerId);
+    },
+    'click .increment' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayerList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .decrement' : function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayerList.update(selectedPlayer, {$inc: {score: -5} });
     }
   });
 }
