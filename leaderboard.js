f5dbf6e593831b7216bde5fce71fef88bb18d785
David Sok <sokd88@gmail.com>
2015-04-08 13:25:01 -0400

    learning templates and events

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,15 @@
 PlayerList = new Mongo.Collection('players');
 
+if(Meteor.isClient){
+  //this code only runs on the client
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayerList.find()
+    },
+  });
+  Template.leaderboard.events({
+    'click .player' : function(){
+      console.log("You clicked a .player element");
+    }
+  });
+}
