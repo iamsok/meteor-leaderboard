ec7ef3a3967432803b2d86b120353df9890d8cd3
David Sok <sokd88@gmail.com>
2015-04-09 10:13:25 -0400

    autopublish

--- leaderboard.js
+++ leaderboard.js
@@ -1,10 +1,12 @@
 PlayerList = new Mongo.Collection('players');
+PlayerList.find().fetch();
 
 if(Meteor.isClient){
   //this code only runs on the client
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayerList.find({}, {sort: {score: -1, name: -1}})
+      var currentUserId = Meteor.userId();
+      return PlayerList.find({}, {sort: {score: -1, name: -1}});
     },
     'selectedClass' : function(){
       var playerId = this._id;
@@ -42,12 +44,20 @@ if(Meteor.isClient){
     'submit form': function(){
         event.preventDefault();
         var playerNameVar = event.target.playerName.value;
+        var currentUserId = Meteor.userId();
         PlayerList.insert({
           name: playerNameVar,
-          score: 0
+          score: 0,
+          createdBy: currentUserId
         });
     }
   })
-
+  Meteor.subscribe('thePlayers');
+}
+if(Meteor.isServer){
+Meteor.publish('thePlayers', function(){
+  var currentUserId = this.userId;
+  return PlayerList.find({createdBy: currentUserId})
+  });
 }
 
